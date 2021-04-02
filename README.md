7072df09b507522531909d19aa513bf8bb48286b
AntonVanke <fjs0801@gmail.com>
2021-04-02 18:07:10 +0800

    第一个版本

--- README.md
+++ README.md
@@ -25,11 +25,50 @@
 
       - Chrome
         - 首先访问`chrome://version/`查看浏览器的版本
-        - 去访问`http://chromedriver.storage.googleapis.com/index.html`下载对应的`webdriver`放到`drivers`下
+        - 去访问<http://chromedriver.storage.googleapis.com/index.html>下载对应的`webdriver`放到`drivers`下
       - Edge
         - 访问`edge://version/`查看浏览器的版本
-        - `https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/`下载对应的`webdriver`放到`drivers`下
+        - <https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/>下载对应的`webdriver`放到`drivers`下
       - Firefox
         - 访问`about:version`查看浏览器的版本
-        - `https://github.com/mozilla/geckodriver/releases/`下载对应的`webdriver`放到`drivers`下
+        - <https://github.com/mozilla/geckodriver/releases/>下载对应的`webdriver`放到`drivers`下
+
+   4. 运行
+
+       1. 在本目录下打开终端运行`python main.py`
+
+           >   第一次打开时会生成几个文件，都是可以删除的。第一次登录会提示你扫码登录，以后的登录都可以通过`cookie.json`登录，所以请妥善保管你的`cookies`
+
+3. ### 一些问题
+
+    -   为什么我的积分没有增加呢？
+
+        >   由于京东的店铺比较多，程序是通过遍历所有店铺，选择赠送京豆的店铺加入，所以进程会很缓慢。
+        >
+        >   在你以前没有用过同类软件的情况下是可以在 10 个小时获得大约 2000 京豆的（具体以各个店铺活动为准），
+        >
+        >   如果你以前用过此类软件，可能京豆**回报率会很低**
+
+    -   为什么我运行报错了？
+
+        >   可能有以下原因
+        >
+        >   1.  没有下载驱动放到`drivers`文件夹里，驱动的版本不对
+        >
+        >       >   请查看文档的第 2 部分安装相应的驱动
+        >
+        >   2.  你的浏览器版本过低
+        >
+        >       >   过低的浏览器版本可能并不支持`selenium 3`或者`headless`模式请你升级到最新版本，并使用对应版本的驱动
+        >
+        >   3.  程序设计的问题
+        >
+        >       >   很抱歉，由于我是匆忙的写完这个爬虫，可能有很多的不足之处，欢迎提交`issues`和`pull requests`
+
+    -   运行后生成了几个文件，都是什么？
+
+        >   -   url.txt: 是遍历的具有入会送豆的页面 `url`
+        >   -   cookie.json: 是你登录京东的`Cookie`**请不要泄露給其他人**
+        >   -   log.txt: 日志文件
+
 
---
+++ main.py
@@ -0,0 +1,196 @@
+import re
+import os
+import time
+import json
+
+from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def print_log(info_type="", title="", info=""):
+    """
+    :param info_type: 日志的等级
+    :param title: 日志的标题
+    :param info: 日志的信息
+    :return:
+    """
+    try:
+        current_url = browser.current_url
+    except:
+        current_url = "浏览器已关闭"
+
+    now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    log = now + "  " + info_type + "  " + title + "  " + info
+    if info_type == "ERROR":
+        print("\033[0;31m" + log + "\033[0m\n")
+    elif info_type == "INFO":
+        print(log)
+    else:
+        print(log)
+        return
+    with open("log.txt", "a", encoding="utf-8") as log_a_file_io:
+        log_a_file_io.write(log + "  " + current_url + "\n")
+
+
+def check_login():
+    """
+    判断当前是否正确登录
+    :return: False: 没有登录成功, True: 登录成功
+    """
+    try:
+        # 如果有用户名了就输出
+        nickname = browser.find_element_by_class_name('nickname')
+        print_log("INFO", "登陆成功", "用户名" + nickname.text)
+        # 保存到 cookie.json
+        with open("cookie.json", "w", encoding="utf-8") as cookie_file_io:
+            cookie_file_io.write(json.dumps(browser.get_cookies()))
+            cookie_file_io.close()
+        return True
+    except:
+        # 如果没有用户名就判定为登录失败
+        return False
+
+
+def login_by_user():
+    """
+    用户手动登录
+    :return: True: 登录成功, False: 登录失败
+    """
+    # 手动登录提示
+    print_log("ERROR", "COOKIE信息不正确", "请手动登录")
+    # 打开登录页面
+    browser.get("https://passport.jd.com/new/login.aspx")
+    # 等待登录成功
+    for _ in range(10):
+        time.sleep(3)
+        if check_login():
+            return True
+    print_log("ERROR", "登录超时", "请在 30s 内登录京东")
+    return False
+
+
+def brand_member(shop_ID):
+    """
+    自动入会
+    :param shop_ID: 店铺ID
+    :return:
+    """
+    url = "https://mall.jd.com/shopBrandMember-" + str(shop_ID) + ".html"
+    browser.get(url)
+    print_log("DEBUG", "访问店铺链接", url)
+    try:
+        gift_info = browser.find_element_by_xpath('//*[@id="J_brandMember"]/div[3]/div/ul')
+
+        # 判断入会是否赠送京豆
+        if len(re.findall("京豆", gift_info.text)):
+            # re.match(r'(\d+)京豆', gift_info.text, re.M | re.I).group(1)
+            jd = int(re.match(r'(\d+)京豆', gift_info.text, re.M | re.I).group(1))
+            url_info = [{'url': url, 'gift': jd}]
+            with open("url.txt", "a", encoding="utf-8") as url_txt_io:
+                url_txt_io.write(str(url_info) + "\n")
+                url_txt_io.close()
+            # 入会
+            checkbox = browser.find_element_by_xpath('//*[@id="J_brandMember"]/div[2]/div/div[3]/p/span[1]')
+            if not checkbox.is_selected():
+                checkbox.click()
+                # 通过class找到按钮并点击成为会员
+                wait.until(
+                    EC.presence_of_element_located((By.XPATH, '//*[@id="J_brandMember"]/div[2]/div/div[4]'))).click()
+                # 计算获取的京豆
+                global get_jd
+                get_jd += jd
+                print_log("INFO", "入会成功", "获得" + str(jd) + "京豆")
+
+    except Exception as e:
+        # print_log("ERROR", "入会失败", str(e.args))
+        pass
+
+
+def task_main():
+    """
+    自动入会主任务
+    :return: None
+    """
+    # 读取店铺ID
+    with open("../../Desktop/shopId.txt", "r", encoding="utf-8") as id_file_io:
+        shop_IDs = id_file_io.readlines()
+        for shop_ID in shop_IDs:
+            brand_member(int(shop_ID))
+
+
+def login_by_file():
+    """
+    通过 cookie.json 登录京东
+    """
+    if os.path.exists("cookie.json"):
+        with open("cookie.json", "r", encoding="utf-8") as config_file_io:
+            try:
+                config_cookies = json.loads(config_file_io.read())
+                # 首先删除旧有的 Cookies
+                browser.delete_all_cookies()
+                # 在浏览器写入新的 Cookies
+                for config_cookie in config_cookies:
+                    browser.add_cookie(config_cookie)
+                # 刷新页面
+                browser.refresh()
+                config_file_io.close()
+                # 如果判断登录成功
+                if check_login():
+                    return True
+            except json.decoder.JSONDecodeError:
+                # cookie 文件不正确
+                print_log("ERROR", "Cookie文件损坏", "自动跳转到手动登录")
+            finally:
+                config_file_io.close()
+    # 如果没有登录成功就需要用户手动登录
+    return login_by_user()
+
+
+if __name__ == '__main__':
+    # 初始化生成文件
+    # cookie.json: 用于记录登录信息
+    # log.txt: 用于记录日志
+    # url.json: 用于记录 url 链接
+    if not os.path.exists("log.txt"):
+        with open("log.txt", "a", encoding="utf-8") as log_file_io:
+            log_file_io.close()
+    if not os.path.exists("url.json"):
+        with open("url.txt", "a", encoding="utf-8") as url_file_io:
+            url_file_io.close()
+
+    # 如果你安装了相应的浏览器请把相应的注释取消，并将其他注释添加，记得修改文件名字
+    # #### Windows  #####
+    # Edge 浏览器
+    # browser = webdriver.Edge("./drivers/msedgedriver.exe")
+    # Chrome 浏览器
+    # browser = webdriver.Chrome("./drivers/chromedriver.exe")
+    # #### MAC OS   #####
+    # Edge 浏览器
+    browser = webdriver.Edge(executable_path="./drivers/msedgedriver", capabilities={})
+    # Chrome 浏览器
+    # browser = webdriver.Chrome("./drivers/chromedriver")
+
+    # 设置等待时间
+    wait = WebDriverWait(browser, 3)
+    # 访问京东页面
+    browser.get("http://www.jd.com")
+    # 计算获得的京豆数
+    get_jd: int = 0
+    print_log("INFO", "开始运行", "--------------------------")
+    # 登录
+    try:
+        if login_by_file():
+            task_main()
+    except Exception as e:
+        print_log("ERROR", "错误", str(e.args))
+    finally:
+        # 退出浏览器
+        print_log("INFO", "本次运行", "获得" + str(get_jd) + "京豆")
+        print_log("INFO", "运行结束", "--------------------------")
+        try:
+            browser.close()
+        except:
+            pass
