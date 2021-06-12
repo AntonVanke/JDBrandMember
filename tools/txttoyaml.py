17e372a07e60892d038aaf19ec7c6e96e030e842
万万 <15863465252@163.com>
2021-06-12 09:32:45 +0800

    add tools/txttoyaml.py

---
+++ tools/txttoyaml.py
@@ -0,0 +1,15 @@
+import datetime
+
+'''
+将shopid.txt文件的内容格式化到shopid.yaml并保存
+'''
+with open('../shopid.yaml','a',encoding='utf-8') as f:
+    with open('shopid.txt') as file_obj:
+        f.write("shop_id:")
+        content = file_obj.read()
+        a = content.split("\n")
+        print(a)
+        for index in a:
+            text = f'\n- \'{index}\''
+            f.write(text)
+        f.write(f"\nupdate_time: {str(datetime.date.today())}")
